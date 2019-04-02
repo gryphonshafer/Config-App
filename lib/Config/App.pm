1e0f0ef4bcb29029ef72192e8eda58514b2ce517
Gryphon Shafer <gryphon@goldenguru.com>
2019-04-02 11:01:28 -0700

    Throw errors and stop on invalid parsing of configuration files

--- lib/Config/App.pm
+++ lib/Config/App.pm
@@ -38,7 +38,16 @@ sub import {
     }
 
     unshift( @INC, $root_dir . "/$_" ) for ( @libs || 'lib' );
-    eval { $self->new($location) };
+
+    my $error = do {
+        local $@;
+        eval {
+            $self->new($location);
+        };
+        $@;
+    };
+
+    die $error . "\n" if ($error);
     return;
 }
 
@@ -85,14 +94,17 @@ sub put {
     my $new_value = pop;
     my $path      = [@_];
     my $node      = pop @{$path};
-
-    eval {
-        my $data = $self->{_conf};
-        $data = $data->{$_} for ( @{$path} );
-        $data->{$node} = $new_value;
+    my $error     = do {
+        local $@;
+        eval {
+            my $data = $self->{_conf};
+            $data = $data->{$_} for ( @{$path} );
+            $data->{$node} = $new_value;
+        };
+        $@;
     };
 
-    return ($@) ? 0 : 1;
+    return ($error) ? 0 : 1;
 }
 
 sub conf {
@@ -245,41 +257,38 @@ sub _find_root_dir {
 
         my ( $config, @errors );
         for my $type (@types) {
-            if (
-                my $error = do {
-                    local $@;
-                    eval {
-                        if ( $type eq 'json' ) {
-                            $json_xs ||= JSON::XS->new
-                                ->utf8
-                                ->relaxed
-                                ->allow_nonref
-                                ->allow_unknown
-                                ->allow_blessed
-                                ->allow_tags;
+            my $error = do {
+                local $@;
+                eval {
+                    if ( $type eq 'json' ) {
+                        $json_xs ||= JSON::XS->new
+                            ->utf8
+                            ->relaxed
+                            ->allow_nonref
+                            ->allow_unknown
+                            ->allow_blessed
+                            ->allow_tags;
 
-                            $config = $json_xs->decode($raw_config);
-                        }
-                        else {
-                            $config = YAML::XS::Load($raw_config);
-                        }
-                    };
-                    $@;
-                }
-            ) {
-                push(
-                    @errors,
-                    'Failed to parse '
-                        . join( ' -> ', map { "\"$_\"" } @source_path, $location )
-                        . $_,
-                );
+                        $config = $json_xs->decode($raw_config);
+                    }
+                    else {
+                        $config = YAML::XS::Load($raw_config);
+                    }
+                };
+                $@;
+            };
+
+            if ($error) {
+                my $message =
+                    'Failed to parse ' .
+                    join( ' -> ', map { "\"$_\"" } @source_path, $location ) . '; ' .
+                    $error;
+                croak($message) if ( not $config );
+                carp($message);
             }
 
             last if ($config);
         }
-
-        croak @errors if ( @errors and not $config );
-        carp @errors if (@errors);
 
         return $config;
     }
